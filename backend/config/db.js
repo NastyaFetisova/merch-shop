50b7802799d03b25b56e4db6a6cf03bd2352e04c
Nastya <fet_nas@mail.ru>
2026-06-27 19:44:57 +0300

    fix: backend/config/db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,25 @@
 const mysql = require('mysql2/promise');
-const databaseConfig = require('./database');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = mysql.createPool({
-    ...databaseConfig,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+
     waitForConnections: true,
     connectionLimit: 10,
+
+
+    ssl: isProduction
+        ? {
+            rejectUnauthorized: false
+        }
+        : undefined
 });
 
 module.exports = pool;
